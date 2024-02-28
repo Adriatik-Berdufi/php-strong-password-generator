8eda0a57bf6956ff18a7ca9e30379d989e24b701
Adriatik Berdufi <adriatik.berdufi@gmail.com>
2024-02-28 16:21:12 +0100

    add str da utilizzare

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
+   
+    //str alfabeto
+    $alfabeto = range('a', 'z');
+    $stringa_alfabeto = implode('', $alfabeto);
+    //str nr
+    $numeri = range(0,9);
+    $stringa_numeri = implode('',$numeri);
+    //str caratteri speciali 
+    $caratteri_speciali = '±§!@#$%^&*()-_=+[]{}|;:,.<>?/|';
 
+    var_dump($stringa_alfabeto); 
+    var_dump($stringa_numeri);
+    var_dump($caratteri_speciali);
 ?>
 
 <!DOCTYPE html>
@@ -10,6 +22,6 @@
     <title>Document</title>
 </head>
 <body>
-   
+
 </body>
 </html>
